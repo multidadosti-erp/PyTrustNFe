1daeed06bbc1c34375c4d1f6768ecb2644e52a6c
Michell Stuttgart <michellstut@gmail.com>
2020-09-08 17:28:22 -0300

    [FIX] Corrige urls dos testes

--- tests/test_servidores.py
+++ tests/test_servidores.py
@@ -7,8 +7,7 @@ Created on Jun 14, 2015
 import unittest
 from pytrustnfe.Servidores import localizar_url, localizar_qrcode
 
-url_ba = 'https://nfe.sefaz.ba.gov.br/webservices/NfeAutorizacao/NfeAutoriza\
-cao.asmx'
+url_ba = 'https://nfe.sefaz.ba.gov.br/webservices/NFeAutorizacao4/NFeAutorizacao4.asmx'  # noqa
 
 url_sp = 'https://nfe.fazenda.sp.gov.br/ws/nfeautorizacao4.asmx'
 
@@ -16,12 +15,11 @@ url_qrcode_homologacao_sp = 'https://homologacao.nfce.fazenda.sp.gov.br/NFCECons
 
 url_sc = 'https://nfe.svrs.rs.gov.br/ws/NfeAutorizacao/NFeAutorizacao4.asmx'
 
-url_rs = 'https://nfe.sefazrs.rs.gov.br/ws/NfeAutorizacao/NFeAutorizacao.asmx'
+url_rs = 'https://nfe.sefazrs.rs.gov.br/ws/NfeAutorizacao/NFeAutorizacao4.asmx'
 
-url_cad_rs = 'https://cad.sefazrs.rs.gov.br/ws/cadconsultacadastro/cadcon\
-sultacadastro2.asmx'
+url_cad_rs = 'https://cad.sefazrs.rs.gov.br/ws/cadconsultacadastro/cadconsultacadastro4.asmx'  # noqa
 
-url_cad_sc = 'https://cad.svrs.rs.gov.br/ws/cadconsultacadastro/cadconsultacadastro4.asmx' # noqa
+url_cad_sc = 'https://cad.svrs.rs.gov.br/ws/cadconsultacadastro/cadconsultacadastro4.asmx'  # noqa
 
 
 class test_servidores(unittest.TestCase):
