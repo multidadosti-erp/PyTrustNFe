8d02be9dcb061b8e21da186513e570126cac443f
Michell Stuttgart <michellstut@gmail.com>
2023-08-02 14:14:51 -0300

    Corrige envio de NFSe de BH

--- pytrustnfe/nfse/bh/__init__.py
+++ pytrustnfe/nfse/bh/__init__.py
@@ -16,34 +16,49 @@ def _render(certificado, method, **kwargs):
     path = os.path.join(os.path.dirname(__file__), "templates")
     xml_send = render_xml(path, "%s.xml" % method, True, **kwargs)
 
+    if not isinstance(xml_send, str):
+        xml_send = etree.tostring(xml_send)
+
+    # Este metodo nao precisa de assinatura
+    if method == 'ConsultarNfsePorRps':
+        return xml_send
+
     reference = ""
     ref_lote = ""
+
     if method == "GerarNfse":
         reference = "rps:%s" % kwargs["rps"]["numero"]
         ref_lote = "lote%s" % kwargs["rps"]["numero_lote"]
+
     elif method == "CancelarNfse":
         reference = "pedidoCancelamento_%s" % kwargs["cancelamento"]["numero_nfse"]
 
     signer = Assinatura(certificado.pfx, certificado.password)
-    xml_send = signer.assina_xml(xml_send, reference)
+
+    xml_send = signer.assina_xml(etree.fromstring(xml_send), reference)
+
     if ref_lote:
         xml_send = signer.assina_xml(etree.fromstring(xml_send), ref_lote)
-    return xml_send.encode("utf-8")
+
+    return xml_send
 
 
 def _send(certificado, method, **kwargs):
     base_url = ""
+
     if kwargs["ambiente"] == "producao":
         base_url = "https://bhissdigital.pbh.gov.br/bhiss-ws/nfse?wsdl"
     else:
         base_url = "https://bhisshomologa.pbh.gov.br/bhiss-ws/nfse?wsdl"
 
-    xml_send = kwargs["xml"].decode("utf-8")
+    xml_send = kwargs["xml"]
     xml_cabecalho = '<?xml version="1.0" encoding="UTF-8"?>\
     <cabecalho xmlns="http://www.abrasf.org.br/nfse.xsd" versao="1.00">\
     <versaoDados>1.00</versaoDados></cabecalho>'
 
-    cert, key = extract_cert_and_key_from_pfx(certificado.pfx, certificado.password)
+    cert, key = extract_cert_and_key_from_pfx(
+        certificado.pfx, certificado.password)
+
     cert, key = save_cert_key(cert, key)
 
     session = Session()
@@ -77,3 +92,13 @@ def cancelar_nfse(certificado, **kwargs):
     if "xml" not in kwargs:
         kwargs["xml"] = xml_cancelar_nfse(certificado, **kwargs)
     return _send(certificado, "CancelarNfse", **kwargs)
+
+
+def xml_consultar_nfse_por_rps(certificado, **kwargs):
+    return _render(certificado, "ConsultarNfsePorRps", **kwargs)
+
+
+def consultar_nfse_por_rps(certificado, **kwargs):
+    if "xml" not in kwargs:
+        kwargs["xml"] = xml_consultar_nfse_por_rps(certificado, **kwargs)
+    return _send(certificado, "ConsultarNfsePorRps", **kwargs)
